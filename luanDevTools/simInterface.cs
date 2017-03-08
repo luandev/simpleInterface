db26376f4b9ea700d334e5b8a9713ad99961a892
Luan Citta <lcitta@overgrounddata.com>
2017-03-08 08:47:34 +0000

    Making it .NET core changing namespace

--- luanDevTools/simInterface.cs
+++ luanDevTools/simInterface.cs
@@ -3,12 +3,14 @@ using System.Collections.Concurrent;
 using System.Diagnostics;
 using System.Threading.Tasks;
 
-namespace simInterface
+namespace luanDevTools
 {
     public class simInterface
     {
-        private ConcurrentDictionary<string, string> listValues = new ConcurrentDictionary<string, string>();
-        public long currentValue
+        /// <summary>
+        /// Progress Bar Minimum value
+        /// </summary>
+        public long progressBarCurrent
         {
             get
             {
@@ -20,8 +22,10 @@ namespace simInterface
             }
         }
 
-
-        public long totalValue
+        /// <summary>
+        /// Progress Bar maximum value
+        /// </summary>
+        public long progressBarMaximum
         {
             get
             {
@@ -32,13 +36,14 @@ namespace simInterface
                 lock (lockpad) { _totalValue = Convert.ToInt32(value); }
             }
         }
-        public bool drawing { get; set; }
-        public bool exit { get; set; }
+        public bool isDrawing { get; set; }
+        private bool isRunning { get; set; }
         public int FPS { get; set; }
 
         private volatile int _totalValue;
         private volatile int _currentValue;
         private string header;
+        private ConcurrentDictionary<string, string> listValues = new ConcurrentDictionary<string, string>();
 
 
         private string spinner = "▲►▼◄";
@@ -47,26 +52,46 @@ namespace simInterface
 
         private Stopwatch sleepTimer = new Stopwatch();
 
-
-        public simInterface(string h, int fps = 4)
+        /// <summary>
+        /// Start the interface drawing, remember to call a Stop method to stop it's rendering
+        /// </summary>
+        /// <param name="Header">Interface title (it cannot be changed)</param>
+        /// <param name="fps">Redrawing frequency</param>
+        public simInterface(string Header, int fps = 4)
         {
-            totalValue = currentValue = 1;
-            drawing = exit = true;
-            header = h;
+            progressBarMaximum = progressBarCurrent = 1;
+            isDrawing = isRunning = true;
+            header = Header;
             FPS = fps;
 
             Task.Factory.StartNew(redraw, TaskCreationOptions.LongRunning).Wait();
         }
 
-        public void addProp(string key, string value)
+        /// <summary>
+        /// Properties to be listed in the interface
+        /// </summary>
+        /// <param name="key">Description</param>
+        /// <param name="value">Value</param>
+        public void addProperty(string key, string value)
         {
             listValues.AddOrUpdate(key, value, (k, o) => value);
         }
 
+
+        /// <summary>
+        /// Disables interface
+        /// </summary>
+        public void stop()
+        {
+            isDrawing = isRunning = false;
+        }
+
+
+
         private void redraw()
         {
-            while (exit)
-                if (drawing)
+            while (isRunning)
+                if (isDrawing)
                 {
                     lock (lockpad)
                     {
@@ -95,23 +120,29 @@ namespace simInterface
 
                         Console.WriteLine();
 
-                        Console.WriteLine("    Total Items = " + totalValue.ToString());
-                        Console.WriteLine("    Done Items  = " + currentValue.ToString());
+                        Console.WriteLine("    Total Items = " + progressBarMaximum.ToString());
+                        Console.WriteLine("    Done Items  = " + progressBarCurrent.ToString());
 
-                        Console.WriteLine();
-                        Console.WriteLine();
-                        Console.WriteLine("    " + percent.ToString() + "%");
-                        Console.WriteLine();
 
-                        string output = string.Empty;
-                        for (var x = 0; x < 76; x++)
+                        if(progressBarMaximum > 1 && progressBarCurrent > 1)
                         {
-                            output += (x < 4) ? " " : (x > pereight) ? "░" : "█";
+                            Console.WriteLine();
+                            Console.WriteLine();
+                            Console.WriteLine("    " + percent.ToString() + "%");
+                            Console.WriteLine();
+
+                            string output = string.Empty;
+                            for (var x = 0; x < 76; x++)
+                            {
+                                output += (x < 4) ? " " : (x > pereight) ? "░" : "█";
+                            }
+
+                            Console.WriteLine(output);
+                            Console.WriteLine();
+                            Console.WriteLine();
                         }
 
-                        Console.WriteLine(output);
-                        Console.WriteLine();
-                        Console.WriteLine();
+                        
 
                         if (indexSpinner > spinner.Length - 1)
                             indexSpinner = 1;
